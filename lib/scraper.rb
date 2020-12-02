c4356ec09e3d7028495d13c318c2a0870ee0ba10
Mike Wilder <mikewilder01@gmail.com>
2020-12-02 14:03:19 -0500

    commit tree 13f2e064245efca31021f9e44d7c21f406836c20

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,11 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    a = Nokogiri::HTML(URI.open(index_url))
   end
 
   def self.scrape_profile_page(profile_url)
